cf88883e416da4e2828eb749e36f5ccdc8d0c9fa
zw0125 <ziweihuang01@gmail.com>
2024-12-03 17:58:50 +0800

    .

--- assignment8.java
+++ assignment8.java
@@ -5,6 +5,7 @@ public class assignment8 {
     private int age;
     private double height;
     private double weight;
+    private double bmi;
 
     // Getters
 
@@ -22,6 +23,10 @@ public class assignment8 {
 
     public double getWeight(){
         return weight;
+    }
+
+    public double getBMI() {
+        return bmi;
     }
 
     // Setters
@@ -48,6 +53,15 @@ public class assignment8 {
         }
     }
 
+    public void setBMI(double bmi) {
+        this.bmi = bmi;
+    }
+
+    public double calculateBMI(double height, double weight) {
+        double bmi = weight / (height*height);
+        return bmi;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         assignment8 person = new assignment8();
@@ -58,13 +72,15 @@ public class assignment8 {
         System.out.print("Enter age: ");
         person.setAge(scanner.nextInt());
 
-        System.out.print("Enter height in cm: ");
+        System.out.print("Enter height in m: ");
         person.setHeight(scanner.nextDouble());
 
         System.out.print("Enter weight in kg: ");
         person.setWeight(scanner.nextDouble());
 
-        double bmi = (person.getHeight()-100)/person.getWeight()  ;
+        //double bmi = person.getWeight()/(person.getHeight()*person.getHeight());
+        double bmi = person.calculateBMI(person.getHeight(), person.getWeight());
+        person.setBMI(bmi);
 
         System.out.println("The person name is " + person.getName() + " and he/she is " + person.getAge() + " years old and his/her bmi is " + bmi);
 
